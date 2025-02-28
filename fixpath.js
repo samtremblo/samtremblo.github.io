72770a3120bf81a2f0235bc7721fab8e7165855e
samuel tremblay <samuel.tremblay@blvd-mtl.com>
2025-02-28 09:33:31 -0500

    ci/cd includes CNAME now for propper redirect

--- fixpath.js
+++ fixpath.js
@@ -1,21 +1,19 @@
-const fs = require('fs');
+
+
+async function createCNAME() {
+  const fs = require('fs');
 const path = require('path');
 
-function replaceInFiles(dir, searchValue, replaceValue) {
-    return null
-  fs.readdirSync(dir).forEach(file => {
-    const filePath = path.join(dir, file);
-    if (fs.lstatSync(filePath).isDirectory()) {
-      replaceInFiles(filePath, searchValue, replaceValue);
-    } else {
-      let content = fs.readFileSync(filePath, 'utf8');
-      let updatedContent = content.replace(new RegExp(searchValue, 'g'), replaceValue);
-      fs.writeFileSync(filePath, updatedContent, 'utf8');
-    }
-  });
+const cnameContent = 'stremblay.cc';
+const cnamePath = path.join(__dirname, 'dist', 'CNAME');
+
+try {
+  fs.writeFileSync(cnamePath, cnameContent, 'utf8');
+  console.log('CNAME file created successfully!');
+} catch (error) {
+  console.error('Error creating CNAME file:', error);
 }
 
-replaceInFiles('./dist', '/public/', '/');
-console.log('❌ NOT USED ANYMORE, SHOULD BE REMOVED');
+}
 
-//console.log('✅ Fixed asset paths in dist/');
+createCNAME();
